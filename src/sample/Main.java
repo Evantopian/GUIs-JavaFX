a840d035c53bf9401607a8cf451358cab43b48c9
Evantopian <evanacademics@gmail.com>
2021-04-30 00:15:31 -0400

    Updated bg, cd, algo

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,4 @@
 package sample;
-import com.sun.javafx.iio.ImageStorage;
 import javafx.application.Application;
 import javafx.event.Event;
 import javafx.event.EventHandler;
@@ -11,93 +10,123 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class Main extends Application implements EventHandler {
 
-    private Scene scene, scene2;
-
+    private Scene scene2;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) {
 
+        // Begins the stack for UI structuring.
+        // Setting the ID for the root for stylesheet purposes.
         StackPane root = new StackPane();
         root.setId("root");
 
-        int pos;
-
+        // Adding the Logo the GUI.
         Image logo = new Image("/Imgs/ticIcon.png");
         ImageView logoImg = new ImageView(logo);
+        logoImg.setTranslateY(-130);    // Positioning
 
-        logoImg.setTranslateY(-130);
-
+        // This image will act as an even handler--sharing the characteristics of a button.
         Image play = new Image("/Imgs/ticPlay.png");
         ImageView playImg = new ImageView(play);
-
         playImg.setTranslateY(75);
 
+        // Adding previous nodes into the UI.
         root.getChildren().addAll(logoImg, playImg);
 
+        // Creating the dimensions for the scene.
+        // The variable is placed here for replaying purposes.
+        Scene scene = new Scene(root, 800, 450);
 
-        TicTacToe game = new TicTacToe();
-        Pane layout = game.getRoot();
-        layout.setId("layout");
 
-
-        // 0 = continue playing, 1 = game has stopped, 2 = stop application.
+        /* Go to lines 108 for a chronological order of the way the program is ran in user perspective.
+           Event handler for when the user clicks on the "Play" Image/Button.
+           Sends the user to a second scene where they will play TicTac toe.
+           TicTacToe class will handle the gameplay.
+         */
         playImg.addEventHandler(MouseEvent.MOUSE_CLICKED, MouseEvent -> {
+            TicTacToe game = new TicTacToe(); // Creates the a TicTacToe object.
+            Pane layout = game.getRoot(); // New pane for a free-styled structured scene.
+            layout.setId("layout"); // Setting the ID again for style-sheet purposes.
 
+            scene2 = new Scene(layout, 800, 450); // Dimensions for scene #2
+            // References the CSS file to set the background for scene #2.
+            scene2.getStylesheets().addAll(this.getClass().getResource("styles.css").toExternalForm());
 
-                primaryStage.setScene(scene2);
+            // Changes the primary scene to scene #2.
+            primaryStage.setScene(scene2);
+            primaryStage.setFullScreen(true); // Fullscreen
 
-                primaryStage.setFullScreen(true);
+            // Overlays for scene #2
+            Image turn = new Image("/Imgs/turn.png");
+            ImageView turnImg = new ImageView(turn);
+            turnImg.setTranslateY(400);
+            turnImg.setTranslateX(150);
 
+            ImageView newLogo = new ImageView(logo);
+            newLogo.setTranslateX(750);
+            newLogo.setTranslateY(200);
+            layout.getChildren().addAll(turnImg, newLogo);
 
-                Image turn = new Image("/Imgs/turn.png");
-                ImageView turnImg = new ImageView(turn);
-                turnImg.setTranslateY(400);
-                turnImg.setTranslateX(150);
-
-                ImageView newLogo = new ImageView(logo);
-                newLogo.setTranslateX(750);
-                newLogo.setTranslateY(200);
-
-                layout.getChildren().addAll(turnImg, newLogo);
-
+            /* User inputs are set as mouse clicks.
+               Calls the run() method from the TicTacToe class.
+               "handleMouseClick()" is simply there to check whether a player has won through the console.
+             */
+            scene2.setOnMouseClicked( e-> {
+                game.handleMouseClick();
                 game.run();
 
-                Image winner = new Image("/Imgs/winnerx.png");
-                ImageView winnerImg = new ImageView(winner);
 
-                Image xImg = new Image("/Imgs/x.png");
-                ImageView xImages = new ImageView(xImg);
-                xImages.setTranslateX(500);
-                xImages.setTranslateY(400);
-                xImages.setFitWidth(75);
-                xImages.setFitHeight(75);
+                // Initializing images for display after win or tie.
+                Image winner;
+                ImageView winnerImg;
 
-                winnerImg.setTranslateX(830);
-                winnerImg.setTranslateY(350);
+                // Calls methods within "game" to check whether a player has won or tied.
+                if (!game.checkWin() || game.checkMaxed()) {
 
-                layout.getChildren().addAll(winnerImg, xImages);
+                    // Conditional for winning.
+                    if (!game.checkWin()) {
+                        if (game.getTurn().equals("x")) {
+                            winner = new Image("/Imgs/winnerX.png");
+                        } else {
+                            winner = new Image("/Imgs/winnerO.png");
+                        }
+                    } //Conditional for a draw game.
+                    else{
+                        winner = new Image("/Imgs/draw.png");
+                    }
 
-                scene2.setOnMouseClicked( e-> {
-                        game.handleMouseClick();
-                });
+                    // After determining the image, it is added and translated.
+                    winnerImg = new ImageView(winner);
+                    winnerImg.setTranslateX(700);
+                    winnerImg.setTranslateY(300);
 
+                    // Creating a new image button for replaying.
+                    Image home = new Image("/Imgs/HomeButton.png");
+                    ImageView homeImg = new ImageView(home);
+                    homeImg.setTranslateX(1500);
+                    homeImg.setTranslateY(700);
+
+                    // Adding previous nodes to a new pane.
+                    layout.getChildren().addAll(winnerImg, homeImg);
+
+                    // Re-directs to the homepage for replaying.
+                    homeImg.setOnMouseClicked(mouseEvent  -> primaryStage.setScene(scene));
+                    primaryStage.setFullScreen(true);
+                }
+            });
         });
 
-        scene = new Scene(root, 800, 450);
+        // References the CSS file to setup the background for scene #1.
         scene.getStylesheets().addAll(this.getClass().getResource("styles.css").toExternalForm());
 
-        scene2 = new Scene(layout, 800, 450);
-        scene2.getStylesheets().addAll(this.getClass().getResource("styles.css").toExternalForm());
-
+        // Setting primary scene to scene #1.
         primaryStage.setScene(scene);
+        primaryStage.setFullScreen(true); // Fullscreen.
         primaryStage.setTitle("TicTacToe");
-        primaryStage.show();
+        primaryStage.show(); // Display scene #1.
     }
-
 
     public static void main(String[] args) {
         launch(args);
@@ -105,6 +134,5 @@ public class Main extends Application implements EventHandler {
 
     @Override
     public void handle(Event event) {
-
     }
 }
